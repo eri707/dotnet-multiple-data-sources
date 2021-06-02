bee06cb48ee65ee0947519b011247c5b3e5054f1
Eri <eri0.sagittarius@gmail.com>
2021-06-01 23:38:37 -0700

    cleaned up code

--- Program.cs
+++ Program.cs
@@ -12,39 +12,31 @@ namespace dotnet_multiple_dataresources
         {
             try
             {
-                using (StreamReader r = new StreamReader("./products.json"))
+                using (StreamReader productReader = new StreamReader("./products.json"))
+                using (StreamReader priceReader = new StreamReader("./productPrices.json"))
+                using (StreamReader discountReader = new StreamReader("./productDiscounts.json"))
+                using (FileStream fileStream = new FileStream("./productsComposites.json", FileMode.Create, FileAccess.Write, FileShare.None))
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
                 {
-                    var jsonString = r.ReadToEnd();
+                    var jsonString = productReader.ReadToEnd();
                     var productsList = JsonSerializer.Deserialize<List<Product>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                    using (StreamReader r2 = new StreamReader("./productPrices.json"))
+                    var jsonString2 = priceReader.ReadToEnd();
+                    var priceList = JsonSerializer.Deserialize<List<ProductPrice>>(jsonString2, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    var jsonString3 = discountReader.ReadToEnd();
+                    var discountList = JsonSerializer.Deserialize<List<ProductDiscount>>(jsonString3, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    // creates productConmposites with the new formation(new ProductComposite) using Select 
+                    var productConposites = productsList.Select(product => new ProductComposite
                     {
-                        var jsonString2 = r2.ReadToEnd();
-                        var priceList = JsonSerializer.Deserialize<List<ProductPrice>>(jsonString2, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                        using (StreamReader r3 = new StreamReader("./productDiscounts.json"))
-                        {
-                            var jsonString3 = r3.ReadToEnd();
-                            var discountList = JsonSerializer.Deserialize<List<ProductDiscount>>(jsonString3, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                            // creates productConmposites with the new formation(new ProductComposite) using Select 
-                            var productConposites = productsList.Select(product => new ProductComposite
-                            {   // when using Select, the contents{} is always only property
-                                ProductId = product.ProductId,
-                                ProductName = product.ProductName,
-                                Category = product.Category,
-                                // find the prices which correspond with ProductId
-                                Price = priceList?.Find(price => price.ProductId == product.ProductId)?.Price,
-                                // find the discounts which correspond with ProductId
-                                Discount = discountList?.Find(discount => discount.ProductId == product.ProductId)?.Discount
-                            });
-                            var serialize = JsonSerializer.Serialize(productConposites, new JsonSerializerOptions { WriteIndented = true });
-                            using (FileStream f = new FileStream("./productsComposites.json", FileMode.Create, FileAccess.Write, FileShare.None))
-                            {
-                                using (StreamWriter w = new StreamWriter(f))
-                                {
-                                    w.WriteLine(serialize);
-                                }
-                            }
-                        }
-                    }
+                        ProductId = product.ProductId,
+                        ProductName = product.ProductName,
+                        Category = product.Category,
+                        // find the prices which correspond with ProductId
+                        Price = priceList?.Find(price => price.ProductId == product.ProductId)?.Price,
+                        // find the discounts which correspond with ProductId
+                        Discount = discountList?.Find(discount => discount.ProductId == product.ProductId)?.Discount
+                    });
+                    var serialize = JsonSerializer.Serialize(productConposites, new JsonSerializerOptions { WriteIndented = true });
+                    streamWriter.WriteLine(serialize);
                 }
             }
             catch (Exception ex)
